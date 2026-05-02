aa937f9cc0667ba64b1b2e645579a23ae2b64270
Aoi Mawatari <tsuduco@gmail.com>
2026-05-02 15:05:42 +0900

    chore: カスタムドメイン設定用のCNAMEファイルを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://berryrook.github.io/",
+  site: "https://berryrook.com",
+  base: "/",
 });
